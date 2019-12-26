f560dac91292f511adeeb2e7fc8d769791b69f9d
MisakaRxyh <825559915@qq.com>
2019-12-26 11:12:45 +0800

    更离散的点

--- kl_projection.py
+++ kl_projection.py
@@ -17,6 +17,19 @@ c2 = np.random.normal(10, 2, [dimension, size])
 # c2 = np.random.poisson(1, [dimension, size])
 c3 = np.random.normal(20, 4, [dimension, size])
 
+# 更离散的点
+# c1x = c21 = np.random.normal(0, 3, [1, size])
+# c1y = np.random.normal(30, 1, [1, size])
+# c1 = np.concatenate((c1x, c1y), axis=0)
+#
+# c2x = np.random.normal(5, 3, [1, size])
+# c2y = np.random.normal(15, 1, [1, size])
+# c2 = np.concatenate((c2x, c2y), axis=0)
+#
+# c3x = np.random.normal(25, 3, [1, size])
+# c3y = np.random.normal(10, 1, [1, size])
+# c3 = np.concatenate((c3x, c3y), axis=0)
+
 x = np.concatenate((c1, c2, c3), axis=1)
 
 R_R = np.cov(x)
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from function import get_feature_vector
 # 样本数
 size = 100
 # 维数
 dimension = 2
 
-c1 = np.random.normal(0, 1, [dimension, size])
-c2 = np.random.normal(10, 5, [dimension, size])
-c3 = np.random.normal(20, 3, [dimension, size])
+# c1 = np.random.normal(0, 1, [dimension, size])
+# c2 = np.random.normal(10, 5, [dimension, size])
+# c3 = np.random.normal(20, 3, [dimension, size])
+
+c1x = c21 = np.random.normal(0, 3, [1, size])
+c1y = np.random.normal(30, 1, [1, size])
+c1 = np.concatenate((c1x, c1y), axis=0)
+
+c2x = np.random.normal(5, 3, [1, size])
+c2y = np.random.normal(15, 1, [1, size])
+c2 = np.concatenate((c2x, c2y), axis=0)
+
+c3x = np.random.normal(25, 3, [1, size])
+c3y = np.random.normal(10, 1, [1, size])
+c3 = np.concatenate((c3x, c3y), axis=0)
+
 # 求变换矩阵R
 x = np.concatenate((c1, c2, c3), axis=1)
 z = np.dot(x, x.T)
@@ -18,7 +31,8 @@ R_R = np.cov(x, bias=True)
 value, vector = np.linalg.eig(R)
 sort_value = -np.sort(-value)
 sort_index = np.argsort(-value)
-Selected_vector = np.concatenate((vector[:, sort_index[0]].reshape(2, 1)))
+Selected_vector = get_feature_vector(R_R, 1, dimension)
+print(Selected_vector)
 # 画点
 plt.plot(x[0], x[1], '.')
 k = Selected_vector[1] / Selected_vector[0]
